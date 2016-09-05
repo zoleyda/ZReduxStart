6822e7b2342836eb99d009fda48da4d42d8a5448
zoleyda <zole.zambrano@gmail.com>
2016-09-05 20:48:40 -0300

    my first 'hello world' in react

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+//new component. this should produce some HTML
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+	return <div> Hola!</div>
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//generate HTML and put it on the page (the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
