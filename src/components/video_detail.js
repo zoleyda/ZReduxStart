ac2504862287b9c8ff81867245c21215c5de95d8
zoleyda <zole.zambrano@gmail.com>
2016-09-08 11:29:34 -0300

    handling null props

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const VideoDetail = ({video}) => {
+	if (!video) {
+		return <div>Loading...</div>;
+	}
+
 	const videoId = video.id.videoId;
 	const url = `https://www.youtube.com/embed/${videoId}`;
 		//'https://www.youtube.com/embed/' + videoId; same up
@@ -8,7 +12,7 @@ const VideoDetail = ({video}) => {
 	return (
 		<div className="video-detail col-md-8">
 			<div className="embed-responsive embed-responsive-16by9">
-				<iframe className="embed-responsive-item"></iframe>
+				<iframe className="embed-responsive-item" src={url}></iframe>
 			</div>
 			<div className="details">
 				<div>{video.snippet.title}</div>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCzn9TQMeTSAXGCICvVJkl7oCIwYtm37BI';
 
@@ -25,6 +26,7 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
+				<VideoDetail video={this.state.videos[0]} />
 				<VideoList videos={this.state.videos} />
 			</div>
 		);
